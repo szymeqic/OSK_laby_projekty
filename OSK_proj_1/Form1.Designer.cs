b7b07148e10a545372da0a115ec3b92b18a84c3c
szymeqic <szym18kowalski@gmail.com>
2024-03-24 14:49:28 +0100

    Teścik

---
+++ OSK_proj_1/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace OSK_proj_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            menuStrip1 = new MenuStrip();
+            gugiToolStripMenuItem = new ToolStripMenuItem();
+            kkToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(500, 153);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { gugiToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // gugiToolStripMenuItem
+            // 
+            gugiToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { kkToolStripMenuItem });
+            gugiToolStripMenuItem.Name = "gugiToolStripMenuItem";
+            gugiToolStripMenuItem.Size = new Size(43, 20);
+            gugiToolStripMenuItem.Text = "gugi";
+            // 
+            // kkToolStripMenuItem
+            // 
+            kkToolStripMenuItem.Name = "kkToolStripMenuItem";
+            kkToolStripMenuItem.Size = new Size(180, 22);
+            kkToolStripMenuItem.Text = "kk";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem gugiToolStripMenuItem;
+        private ToolStripMenuItem kkToolStripMenuItem;
+    }
+}

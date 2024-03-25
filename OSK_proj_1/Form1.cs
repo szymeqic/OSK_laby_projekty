473ced92fa635b260cac6449a6272faa34aff7b8
szymeqic <szym18kowalski@gmail.com>
2024-03-25 21:16:52 +0100

    Odpowiednia numeracja pictureboxow

--- OSK_proj_1/Form1.cs
+++ OSK_proj_1/Form1.cs
@@ -1,3 +1,6 @@
+using OSK_proj_1.Properties;
+using System.Text;
+
 namespace OSK_proj_1
 {
     public partial class Form1 : Form
@@ -9,7 +12,7 @@ namespace OSK_proj_1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            //init
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -20,6 +23,19 @@ namespace OSK_proj_1
         private void button2_Click(object sender, EventArgs e)
         {
             this.button2.Text = "Dzia³a";
+        }
+
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            //funkcja gugu
+            PictureBox kafelek = (PictureBox) sender;
+            string tekst = kafelek.Name;
+            this.label1.Text = tekst;
+
+            kafelek.BackgroundImage = Properties.Resources.cat_weed;
+         
+
         }
     }
 }

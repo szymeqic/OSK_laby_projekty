12a0385e92f92f50b148929abf8c969a03ebabe6
MarekZegare4 <106778416+MarekZegare4@users.noreply.github.com>
2024-04-28 15:28:59 +0200

    usuniete gugu

--- OSK_proj_2/Form1.Designer.cs
+++ OSK_proj_2/Form1.Designer.cs
@@ -29,82 +29,82 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            components = new System.ComponentModel.Container();
-            pictureBox1 = new PictureBox();
-            button1 = new Button();
-            label1 = new Label();
-            timer1 = new System.Windows.Forms.Timer(components);
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.BackgroundImage = Properties.Resources.dziecko;
-            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
-            pictureBox1.Cursor = Cursors.SizeAll;
-            pictureBox1.Location = new Point(372, 76);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(195, 172);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
-            pictureBox1.Visible = false;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(713, 259);
-            button1.Name = "button1";
-            button1.Size = new Size(150, 56);
-            button1.TabIndex = 1;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            label1.Location = new Point(732, 143);
-            label1.Name = "label1";
-            label1.Size = new Size(90, 30);
-            label1.TabIndex = 2;
-            label1.Text = "bimbom";
-            label1.Click += label1_Click;
-            // 
-            // timer1
-            // 
-            timer1.Enabled = true;
-            timer1.Interval = 1;
-            timer1.Tick += timer1_Tick;
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackgroundImage = Properties.Resources.tlo;
-            BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(1104, 554);
-            Controls.Add(label1);
-            Controls.Add(button1);
-            Controls.Add(pictureBox1);
-            Name = "Form1";
-            Text = "Test reakcji";
-            Load += Form1_Load;
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ResumeLayout(false);
-            PerformLayout();
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			components = new System.ComponentModel.Container();
+			pictureBox1 = new PictureBox();
+			button1 = new Button();
+			label1 = new Label();
+			timer1 = new System.Windows.Forms.Timer(components);
+			((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+			SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			pictureBox1.BackgroundImage = Properties.Resources.dziecko;
+			pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+			pictureBox1.Cursor = Cursors.SizeAll;
+			pictureBox1.Location = new Point(372, 76);
+			pictureBox1.Name = "pictureBox1";
+			pictureBox1.Size = new Size(195, 172);
+			pictureBox1.TabIndex = 0;
+			pictureBox1.TabStop = false;
+			pictureBox1.Visible = false;
+			// 
+			// button1
+			// 
+			button1.Location = new Point(713, 259);
+			button1.Name = "button1";
+			button1.Size = new Size(150, 56);
+			button1.TabIndex = 1;
+			button1.Text = "button1";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 238);
+			label1.Location = new Point(732, 143);
+			label1.Name = "label1";
+			label1.Size = new Size(114, 30);
+			label1.TabIndex = 2;
+			label1.Text = "Test reakcji";
+			label1.Click += label1_Click;
+			// 
+			// timer1
+			// 
+			timer1.Enabled = true;
+			timer1.Interval = 1;
+			timer1.Tick += timer1_Tick;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackgroundImage = Properties.Resources.tlo;
+			BackgroundImageLayout = ImageLayout.Stretch;
+			ClientSize = new Size(1104, 554);
+			Controls.Add(label1);
+			Controls.Add(button1);
+			Controls.Add(pictureBox1);
+			Name = "Form1";
+			Text = "Test reakcji";
+			Load += Form1_Load;
+			((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
+		#endregion
 
-        private PictureBox pictureBox1;
+		private PictureBox pictureBox1;
         private Button button1;
         private Label label1;
         private System.Windows.Forms.Timer timer1;

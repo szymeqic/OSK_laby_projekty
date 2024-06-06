786010b54446df4ac7dd060bc80b4da6c375efa2
szymeqic <szym18kowalski@gmail.com>
2024-06-06 19:50:06 +0200

    Zminiełem nazwy tych kart zobacz czy coś ci sie zesra

--- Oczko/Form1.Designer.cs
+++ Oczko/Form1.Designer.cs
@@ -6,6 +6,7 @@
         /// Wymagana zmienna projektanta.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        public talia talia;
 
         /// <summary>
         /// Wyczyść wszystkie używane zasoby.
@@ -28,7 +29,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textbox_spisKart = new System.Windows.Forms.TextBox();
+            this.button_pobierzKarte = new System.Windows.Forms.Button();
+            this.picture_karta1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_karta1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // textbox_spisKart
+            // 
+            this.textbox_spisKart.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textbox_spisKart.Location = new System.Drawing.Point(681, 305);
+            this.textbox_spisKart.Multiline = true;
+            this.textbox_spisKart.Name = "textbox_spisKart";
+            this.textbox_spisKart.Size = new System.Drawing.Size(114, 169);
+            this.textbox_spisKart.TabIndex = 0;
+            // 
+            // button_pobierzKarte
+            // 
+            this.button_pobierzKarte.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_pobierzKarte.Location = new System.Drawing.Point(307, 316);
+            this.button_pobierzKarte.Name = "button_pobierzKarte";
+            this.button_pobierzKarte.Size = new System.Drawing.Size(75, 48);
+            this.button_pobierzKarte.TabIndex = 1;
+            this.button_pobierzKarte.Text = "Pobierz kartę";
+            this.button_pobierzKarte.UseVisualStyleBackColor = true;
+            this.button_pobierzKarte.Click += new System.EventHandler(this.button_pobierzKarte_Click);
+            // 
+            // picture_karta1
+            // 
+            this.picture_karta1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picture_karta1.Location = new System.Drawing.Point(541, 296);
+            this.picture_karta1.Name = "picture_karta1";
+            this.picture_karta1.Size = new System.Drawing.Size(110, 169);
+            this.picture_karta1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture_karta1.TabIndex = 2;
+            this.picture_karta1.TabStop = false;
             // 
             // Form1
             // 
@@ -37,13 +72,24 @@
             this.BackgroundImage = global::Oczko.Properties.Resources.Blackjack_table_seats;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(907, 505);
+            this.Controls.Add(this.picture_karta1);
+            this.Controls.Add(this.button_pobierzKarte);
+            this.Controls.Add(this.textbox_spisKart);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Oczko";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picture_karta1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textbox_spisKart;
+        private System.Windows.Forms.Button button_pobierzKarte;
+        private System.Windows.Forms.PictureBox picture_karta1;
     }
 }
 
--- Oczko/Form1.cs
+++ Oczko/Form1.cs
@@ -1,20 +1,115 @@
-﻿using System;
+﻿using Oczko.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Oczko
 {
+
+    public enum Kolor { 
+        Pik,
+        Kier,
+        Karo,
+        Trefl
+    }
+
+    public enum Figura
+    {
+        Dwójka =2,
+        Trójka,
+        Czwórka,
+        Piątka,
+        Szóstka,
+        Siódemka,
+        Ósemka,
+        Dziewiątka,
+        Dziesiątka,
+        Walet,
+        Dama,
+        Król,
+        As
+    }
+
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.talia = new talia();
+            for (int i = 0;i<4; i++)          //inicjalizacja talii
+                for (int j = 2; j < 15; j++)
+                    this.talia.czynne.Enqueue(new karta((Kolor)i, (Figura)j));
+
+        }
+
+        private void button_pobierzKarte_Click(object sender, EventArgs e)
+        {
+            karta temp = this.talia.pobierz();
+            this.textbox_spisKart.Text += temp.ToString() + '\n';
+            this.picture_karta1.BackgroundImage = temp.zdj;
+        }
+
+        
+    }
+
+    public class talia
+    {
+        public Queue<karta> odrzucone;
+        public Queue<karta> czynne;
+
+
+
+        public talia(){
+            this.odrzucone = new Queue<karta>();
+            this.czynne = new Queue<karta>();
+        }
+
+        public void tasuj() { }
+
+        public karta pobierz()
+        {
+
+            if (czynne.Count <= 0)
+            {
+                czynne = odrzucone;
+                tasuj();
+            }
+
+            karta temp = czynne.Dequeue();
+            odrzucone.Enqueue(temp);
+            return temp;
+        }
+
+    }
+    public class karta
+    {
+
+        public karta(Kolor kol, Figura fig)
+        {
+            this.kolor = kol;
+            this.figura = fig;
+            this.zdj = Image.FromFile($"..\\..\\Properties\\karty\\{this.figura}_{this.kolor}.png");
+        }
+        public Kolor kolor { get; }
+        public Figura figura { get; }
+
+        public Image zdj { get; }
+
+        public override string ToString()
+        {
+            return $"{this.figura} {this.kolor}";
         }
     }
 }
